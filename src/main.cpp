1d1e27cc034ac44692be891b644260d00d337622
Ondrej Semmler <o.semmler@gmail.com>
2017-10-27 15:22:57 +0200

    Prvni verze Kappa.

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Monster.h>
+
+class Kappa : public Monster
+{
+public:
+  Kappa()
+  {
+    partCount = 1;
+    partArray = new Part*[partCount];
+    partArray[0] = new Part_Light_LM393(PIN_A0, PIN2);
+    //partArray[1] = new Part_DHT(PIN_A1, DHT11);
+  }
+};
+
+Kappa kappa;
+
+void setup()
+{
+  kappa.setup();
+}
+
+void loop()
+{
+  kappa.readAllProps();
+  delay(3000);
+}
