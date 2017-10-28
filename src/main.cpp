79d4d1a97c074a55829a197084290f46f0871694
Ondrej Semmler <o.semmler@gmail.com>
2017-10-28 11:20:18 +0200

    Pridani senzoru DHT

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@ class Kappa : public Monster
 public:
   Kappa()
   {
-    partCount = 1;
+    partCount = 2;
     partArray = new Part*[partCount];
     partArray[0] = new Part_Light_LM393(PIN_A0, PIN2);
-    //partArray[1] = new Part_DHT(PIN_A1, DHT11);
+    partArray[1] = new Part_DHT(PIN_A1, DHT11);
   }
 };
 
