b2add810ff1326ca1d43a421cb89dc8f8440de99
Ondra Semmler <o.semmler@gmail.com>
2018-06-15 17:27:55 +0200

    Novy senzor teplty DS18B20

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,11 @@ class Kappa : public Monster
 public:
   Kappa()
   {
-    partCount = 2;
+    partCount = 3;
     partArray = new Part*[partCount];
     partArray[0] = new Part_Light_LM393(PIN_A0, PIN2);
     partArray[1] = new Part_DHT(PIN_A1, DHT11);
+    partArray[2] = new Part_Temp_DS18B20(8);
   }
 };
 
